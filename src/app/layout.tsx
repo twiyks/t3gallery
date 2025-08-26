d444623e675aa13ef318f45ee9f7063c98ca6378
twiyks <97100372+twiyks@users.noreply.github.com>
2025-08-26 12:35:58 +0100

    scaffold ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,16 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+function TopNav () {
+  return (
+    <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold">
+      <div>Gallery</div>
+      <div>Sign in</div>
+
+    </nav>
+  )
+  }
+
 const geist = Geist({
   subsets: ["latin"],
   variable: "--font-geist-sans",
@@ -18,8 +28,11 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
-      <body>{children}</body>
+    <html lang="en">
+      <body className={`${geist.variable} font-sans`} >
+        <TopNav />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import Link from "next/link";
+import { ur } from "zod/v4/locales";  
+
+const mockUrls = [
+  "https://prt25e8zgf.ufs.sh/f/qM4SByezm6aECr0wSoTV7uqxoflnhSvL8c6XPjBEkwYNdpb3",
+  "https://prt25e8zgf.ufs.sh/f/qM4SByezm6aEdJnQpXGTLWM63ok29jpGHYgzvDfPbOinK1yw",
+];
+
+const mockImages =  mockUrls.map((url, index) => ({
+
+id: index + 1,
+url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages,...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url}/>
+          </div>
+        ))}
+      </div>
+      Hello (gallery in progress 2)
     </main>
   );
 }
